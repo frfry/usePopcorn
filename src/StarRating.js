aca87b57336e5a9aa11d55e07bb97b6646303e17
Kylee T <38272410+frfry@users.noreply.github.com>
2024-05-02 03:38:29 +0000

    Adding Hover events

--- src/StarRating.js
+++ src/StarRating.js
@@ -14,7 +14,8 @@ const starStyle = {
 };
 
 export default function StarRating({ maxRating = 5 }) {
-  cosnt[(rating, setRating)] = useState(0);
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -24,17 +25,29 @@ export default function StarRating({ maxRating = 5 }) {
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star key={1} onRate={() => handleRating(i + 1)} full={rating >= i+1}/>
+          <Star
+            key={1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+          />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onRate}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={7} />
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
