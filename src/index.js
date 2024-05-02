ebfe0d319f5eb7df75a4fc57fcfd840af247b934
Kylee T <38272410+frfry@users.noreply.github.com>
2024-05-02 04:11:15 +0000

    Improve Reusability

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 //import "./index.css";
 //import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      className="test"
+      color="red"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+      size={20}
+    />
+    <Test />
   </React.StrictMode>
 );
